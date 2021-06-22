f121459ea9599ad0010b2d3b8c86cba3c12de1b6
Piyush Goyal <60353847+Piyush-prog@users.noreply.github.com>
2021-06-21 21:33:52 -0400

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({msg:'Welcome to the Contact Keeper API.'})
+})
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+
+const PORT = process.env.PORT || 5000;
+//We use PORT from the environment variable or local port. Also, we've set a callback to see if the server starts successfully.
+app.listen(PORT, ()=> {
+    console.log(`Server Started on Port ${PORT}`);
+});
